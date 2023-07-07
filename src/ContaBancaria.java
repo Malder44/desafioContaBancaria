4620be76502ddb3273aa1686e35a44609d46d419
Leonorico <leonorico2epb@gmail.com>
2023-07-07 15:34:05 -0300

    Criação do esboço da repetição e criação e conclusão da função apresentaMenu

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -2,6 +2,32 @@ import java.util.Scanner;
 
 public class ContaBancaria {
 
+    public static void apresentaMenu() {
+        String menu = """
+                Operações
+                
+                1- Consultar saldos
+                2- Receber valor
+                3- Transferir valor
+                4- Sair
+                
+                Digite a opção desejada:
+                """;
+        System.out.println(menu);
+    }
+
+    public static double consultaSaldo() {
+        return 0;
+    }
+
+    public static double recebeTransferencia() {
+        return 0;
+    }
+
+    public static double fazTransferencia() {
+        return 0;
+    }
+
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         String nomeDoCliente = "Leonorico Borges";
@@ -19,5 +45,26 @@ public class ContaBancaria {
                 """.formatted(nomeDoCliente,tipoConta,saldo);
 
         System.out.println(extratoInicial);
+
+        do {
+            apresentaMenu();
+            switch(opcaoSelecionada) {
+                case 1:
+                    consultaSaldo();
+                    break;
+                case 2:
+                    recebeTransferencia();
+                    break;
+                case 3:
+                    fazTransferencia();
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        } while(opcaoSelecionada != 4);
+
+        System.out.println("Saindo da conta bancária.");
     }
 }
